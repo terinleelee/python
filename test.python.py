6620b1ab7d2e81f65becbba5edad61bacb73c74d
terinleelee <155145768+terinleelee@users.noreply.github.com>
2024-01-05 18:57:02 +0900

    2024-01-05

---
+++ test.python.py
@@ -0,0 +1,162 @@
+##year=int(input("연도:"))
+##month=input("월:")
+##day= input("일:")
+##
+####print("오늘은 %s년 %s월 %s일 입니다" %(year,month,day))
+##print(year+month)
+           
+    
+##L=int(input("L 판매개수 :"))
+##M=int(input("M 판매개수 :"))
+##S=int(input("S 판매개수 :"))
+##
+##sum=2500*L
+##sum=sum+1000*M
+##sum=sum+500*S
+##
+##print("오늘의 총 매출은  %d원입니다." %(sum))
+
+##height=int(input())
+##width=int(input())
+##
+##
+##area=(height*width/2)
+##
+##print(area)
+                                                   
+##a=int(input())
+##b=int(input())
+##
+##print(a**b)
+
+
+
+##a =[10,20,30,40,50]
+##print(a[0])
+##b=[10,20,[30,40],50]
+##print(b[2])
+##print(b[2][0])
+##print(b[2][1])
+
+##a=[10,220,30,40,50]
+##print(a[1:4])
+
+##a=[1,2,'a','b',3,4,'c']
+##
+##print(a[5])
+##print(a[0:2])
+##print(a[4:-1])
+##print(a[0]+a[1])
+##print(a[2]+a[3]+a[6])
+
+##a=[10,20,30,40,50]
+##a[0]='a'
+##print(a)
+##a[1:3]='b'
+##print(a)
+
+##a='I love you'
+##print(a.count('o'))
+##print(a.find('y'),a.index('y'))
+##print('.'.join(a))
+##print(a.split())
+##print(a.replace('Love','like'))
+##print(a.upper(),a.lower())
+
+
+##b='efgh'
+##print('%s%s'%(a, b))
+##a= a.strip()
+##print('%s%s' %(a,b))
+##
+##a='hello world'
+##print(a.count('l'))
+##
+##a='oh!my!god'
+##print(a.split('!'))
+
+
+
+##a=int(input())
+##
+##if(a%2==0):
+##    print("even")
+##else:
+##    print("odd")
+
+##import random
+##
+##com = random.randint(1,2)
+##user=int(input('odd:1,even:2\n'))
+##
+##print('COM(%d) : USER(%d)' %(com,user))
+##if com == user:
+##    print('correct')
+##else:
+##    print('Incorrect')
+
+
+##import random
+##com = random.randint(1,2)
+##user=int(input('odd:1,even:2'))
+##print(com)
+##
+##
+##if (com==1 and user==1):
+##    print('COM(odd) : USER(odd)')
+##    print('correct')
+##    
+##if(com==2 and user==2)
+##    print('COM(EVEN) : USER(EVEN)')
+##    print('incorrect')
+##num=input('주민 번호:')
+##a = num[0:6]
+##
+##
+##b=num[7]
+##print('생년월일: %s' % a)
+##print('성별: %s' %b)
+
+##num=input('주민 번호:')
+##
+##b = num[2:4]
+##c = num[4:6]
+##print('%s월 %s일' % (b,c))
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
